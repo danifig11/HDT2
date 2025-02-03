2b2b77a951b2703c95a3c7f5299279916af6341a
Oliver <olivercifuentes338@gmail.com>
2025-02-03 12:16:20 -0600

    Arraylist
    guardar y usar los datos que se pueden cambiar, dejando usar las operaciones basicas de la pila

---
+++ ArrayListStack.java
@@ -0,0 +1,46 @@
+public class ArrayListStack<T> implements Pila<T> {
+    private Object[] pila;
+    private int tamano;
+
+    public ArrayListStack() {
+        pila = new Object[10]; // Capacidad 
+        tamano = 0;
+    }
+
+    public void push(T elemento) {
+        if (tamano == pila.length) {
+            expandirCapacidad();
+        }
+        pila[tamano++] = elemento;
+    }
+
+    public T pop() {
+        if (estaVacia()) {
+            throw new RuntimeException("La pila esa vacia.");
+        }
+        return (T) pila[--tamano];
+    }
+
+    public T peek() {
+        if (estaVacia()) {
+            throw new RuntimeException("La pila esta vacia.");
+        }
+        return (T) pila[tamano - 1];
+    }
+
+    public boolean estaVacia() {
+        return tamano == 0;
+    }
+
+    public int tamano() {
+        return tamano;
+    }
+
+    private void expandirCapacidad() {
+        Object[] nuevaPila = new Object[pila.length * 2];
+        for (int i = 0; i < pila.length; i++) {
+            nuevaPila[i] = pila[i];
+        }
+        pila = nuevaPila;
+    }
+}
